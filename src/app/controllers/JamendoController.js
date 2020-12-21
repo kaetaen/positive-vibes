1858529ce9d542052891e9add017a419d0dc40dd
kaetaen <kaetaen@tutanota.com>
2020-12-21 12:34:02 -0300

    Add JamendoController

---
+++ src/app/controllers/JamendoController.js
@@ -0,0 +1,18 @@
+const JamendoAPI = require('../../services/JamendoAPI')
+
+class JamendoController {
+  async index (_req, res) {
+    const songs = await JamendoAPI
+    
+    const songList = songs.map((song, _index) => {
+      const { name, artist_name } = song
+      const songInfo = {name, artist_name} 
+
+      return songInfo
+    })
+
+    res.status(200).json({ songs : songList })
+  }
+}
+
+module.exports = new JamendoControler()
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 const routes = Router()
 
+const JamendoController = require('./app/controllers/JamendoController')
 const PensadorController = require('./app/controllers/PensadorController')
 
 routes.get('/quotes', PensadorController.index)
 routes.get('/quotes/random', PensadorController.random)
+
+routes.get('/songs', JamendoController.index)
+
 
 module.exports = routes
