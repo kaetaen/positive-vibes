bd3a99bb56e717a09c0aaa0076ce76b0f82fd6c8
kaetaen <kaetaen@tutanota.com>
2020-12-24 02:36:45 -0300

    Add methods connect and parseImages to Pixabay Service

--- src/services/PixabayAPI.js
+++ src/services/PixabayAPI.js
@@ -1,5 +1,37 @@
+const axios = require('axios')
+require('dotenv').config()
+
 class Pixabay {
   constructor () {
     this.key = process.env.PIXABAY_API_KEY 
   }
+
+  async connect () {
+    const api = await axios({
+      url: 'https://www.pixabay.com/api/',
+      params: {
+        key: this.key,
+        q: 'nature',
+        orientation: 'vertical',
+        category: 'nature',
+        editors_choice: true,
+        per_page: 200
+        
+      }
+    })
+
+    return api
+  }
+
+  async parseImages () {
+    const images = await this.connect()
+    const { hits } = images.data
+    const imageUrls = hits.map((photo, _index) => photo.webformatURL)
+
+    return imageUrls
+  }
 }
+
+const pixabay = new Pixabay()
+
+pixabay.parseImages()
