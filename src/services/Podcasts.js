6debaaa9559ca02332f644eec35616407cb6071a
kaetaen <kaetaen@tutanota.com>
2021-01-01 01:57:49 -0300

    Add service Podcasts........ and H4pp1 Nu Y3ar 2 U ! ! !

---
+++ src/services/Podcasts.js
@@ -0,0 +1,25 @@
+const rssParser = require('rss-parser')
+
+class Podcasts {
+  async connect () {
+    const rss = await new rssParser().parseURL('https://anchor.fm/s/16afada0/podcast/rss')
+    return rss.items
+  }
+
+  async parseEpisodes () {
+    const episodes = await this.connect()
+    const episodesDetails = episodes.map((episode, _index) => {
+      const {
+        title,
+        enclosure: { url }
+      } = episode
+
+      return {
+        title,
+        mediaLink
+      }
+    })
+  }
+}
+
+module.exports = Podcasts
